d6d873903b22951a6bc48ed2d3eb25715401fa2e
stalker-john <hussainmarufbhilai@gmail.com>
2023-01-28 16:22:55 +0530

    added plugins for firebase

--- lib/Methods.dart
+++ lib/Methods.dart
@@ -1,4 +1,4 @@
-import 'package:chet_app/login_page.dart';
+import 'package:chetster/login_page.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
